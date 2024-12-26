f776c65e1981b87e67e042cc4d29e33569c7ee10
caoshiyue <caoshiyueKevin@Gmail.com>
2024-12-26 09:44:00 +0800

    m

--- api_key.py
+++
@@ -1,8 +0,0 @@
-url= 'https://xiaoai.plus/v1'
-key='sk-JRzmgd8eRsNTIpx0B6F9B08025314669B54cBfEe4d769493'
-
-# url="https://chatapi.littlewheat.com/v1" # 备用
-# key="sk-7oU5Syutb4zP8fzllrPV29l5yPhZFHLjLclfLjcTPEg6UHFs"
-
-# https://use.52apikey.cn/v1
-# sk-6oEeZ6AktD4UIvbc1ZqZpN52kvsj7uJ6XTVky7NcUvVGA69C
